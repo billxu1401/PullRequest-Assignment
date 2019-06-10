eadfa51abe300190b54ae177ceea56ae6b949972
billxu1401 <34110282+billxu1401@users.noreply.github.com>
2019-06-10 12:45:35 -0400

    Updated driver
    :)

---
+++ Branching-Assignment/src/Xu_Bill/Driver.java
@@ -0,0 +1,13 @@
+package Xu_Bill;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		System.out.println(Exercises.threeAndFive());
+		System.out.println(Exercises.evenFibSum());
+		System.out.println(Exercises.largestPrimeFactor());
+
+	}
+
+}
